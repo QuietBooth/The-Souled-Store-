94e0dd317d0380e97a1337b34df57bf8351bfcca
Addarshkumar <addarsh.kumar@kalvium.community>
2023-10-31 18:13:26 +0530

    product page

---
+++ Branches/productpage/product.js
@@ -0,0 +1,75 @@
+//first image
+function setNewImage1(){
+  document.querySelector("#image1").src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1698309758_5303029.jpg?format=webp&w=480&dpr=1.3"
+}
+
+function setOldImage1(){
+    document.querySelector("#image1").src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1698309758_5452923.jpg?format=webp&w=480&dpr=1.3"
+  }
+//second image
+
+function setNewImage2(){
+    document.querySelector("#image2").src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1697868176_1230807.jpg?format=webp&w=480&dpr=1.3"
+  }
+  
+function setOldImage2(){
+      document.querySelector("#image2").src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1697868176_6225666.jpg?format=webp&w=480&dpr=1.3"
+}
+
+//third image
+
+function setNewImage3(){
+        document.querySelector("#image3").src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1697894215_6985243.jpg?format=webp&w=480&dpr=1.3"
+    }
+      
+function setOldImage3(){
+          document.querySelector("#image3").src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1698641129_6103640.jpg?format=webp&w=480&dpr=1.3"
+    }
+
+//fourth image
+
+function setNewImage4(){
+        document.querySelector("#image4").src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1698310382_2463321.jpg?format=webp&w=480&dpr=1.3"
+}
+      
+function setOldImage4(){
+          document.querySelector("#image4").src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1698310382_8266695.jpg?format=webp&w=480&dpr=1.3"
+}
+
+//fifth image
+
+function setNewImage5(){
+    document.querySelector("#image5").src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1697864223_8196812.jpg?format=webp&w=768&dpr=1.3"
+}
+  
+function setOldImage5(){
+      document.querySelector("#image5").src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1697864223_5378514.jpg?format=webp&w=1080&dpr=1.3"
+}
+
+//sixth image
+function setNewImage6(){
+    document.querySelector("#image6").src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1698302102_4821864.jpg?format=webp&w=480&dpr=1.3"
+}
+  
+function setOldImage6(){
+      document.querySelector("#image6").src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1698302102_7509138.jpg?format=webp&w=1080&dpr=1.3"
+}
+
+//seventh image
+function setNewImage7(){
+    document.querySelector("#image7").src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1697363725_5491335.jpg?format=webp&w=768&dpr=1.3"
+}
+  
+function setOldImage7(){
+      document.querySelector("#image7").src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1697363725_8182226.jpg?format=webp&w=768&dpr=1.3"
+}
+ //eighth image
+
+ function setNewImage8(){
+    document.querySelector("#image8").src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1698329616_5760309.jpg?format=webp&w=480&dpr=1.3"
+}
+  
+function setOldImage8(){
+      document.querySelector("#image8").src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1698329616_5847977.jpg?format=webp&w=480&dpr=1.3"
+}
+
